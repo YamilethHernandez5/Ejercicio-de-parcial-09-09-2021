900b5013cf6aa78e3198fc91f80db2d84584f63d
JulissaHdzClaros <calebyunissahdz@gmail.com>
2021-09-09 17:02:30 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio de parcial 09-09-2021/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_de_parcial_09_09_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("ADIVINA EL NÚMERO");
+
+            Console.WriteLine("¡Bienvenido a Adivina el número!\n\n Voy a pensar en un número del 0"+
+                " al 10 y debes adivinarlo\n Tienes 3 vidas");
+
+            Random random = new Random(Guid.NewGuid().GetHashCode());
+            int numminimo = 0, nummaximo = 11, cantidaddevidas = 0;
+            int numerosdeljuego = random.Next(numminimo, nummaximo);
+            int numingresado = 0;
+            do
+            {
+                 numingresado = Convert.ToInt32(Console.ReadLine());
+
+                if (numingresado > numerosdeljuego)
+                {
+                    Console.WriteLine("No es el número correcto!");
+                    cantidaddevidas++;
+                }
+
+                else if (numingresado < numerosdeljuego)
+                {
+                    Console.WriteLine("No es el número correcto!");
+                    cantidaddevidas++;
+                }
+                  
+                else
+                {
+                    Console.WriteLine("¡GANASTE!");
+                    Console.ReadKey();
+                }
+
+            } while (cantidaddevidas < 3); 
+
+
+           
+
+        }
+    }
+}
+   
+    
